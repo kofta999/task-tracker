b6a016b590b50fb32ef7e8df8379f8f075dea4a4
kofta999 <mostafaxxx555@gmail.com>
2023-10-09 15:22:48 +0300

    inital commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export default {
+  name: "Footer",
+};
+</script>
+
+<template>
+  <footer>
+    <p>Copyright &copy; 2023</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: antiquewhite;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+  <Button
+    v-show="homePage"
+    @btn-click="$emit('toggle-add-task')"
+    :text="showAddTask ? 'Close' : 'Add Task'"
+    :color="showAddTask ? 'red' : 'green'"
+  />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
